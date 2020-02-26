49d1f0c500ff8186951c482263e9b7a58df15b20
Andy Fry <andrewfalefry@gmail.com>
2020-02-26 14:57:40 -0600

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const moment = require('moment');
+
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addFilter('date', date => {
+      return moment(date).utc().format('LL'); // E.g. May 31, 2019
+    });
+  
+    return {
+      templateFormats: [
+        "md",
+        "png",
+        "jpg",
+        "css" // css is not yet a recognized template extension in Eleventy
+      ]
+    };
+  };
