770f0b413346430d000d54deb44e5f1cfa98ea15
Andy Fry <andrewdalefry@gmail.com>
2020-08-28 06:47:21 -0500

    Updated configuration

--- .eleventy.js
+++ .eleventy.js
@@ -2,19 +2,25 @@ const moment = require('moment');
 
 module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addFilter('date', date => {
-      return moment(date).utc().format('LL'); // E.g. May 31, 2019
-    });
-  
-    return {
-      templateFormats: [
-        "html",
-        "md",
-        "png",
-        "jpg",
-        "jpeg",
-        "pdf",
-        "css" // css is not yet a recognized template extension in Eleventy
-      ]
-    };
+  eleventyConfig.setQuietMode(true);
+  eleventyConfig.setDataDeepMerge(true);
+
+  // Copy directories to _sites
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("files");
+
+  // **************************************************************************
+  // Custom Filters
+  // **************************************************************************
+  eleventyConfig.addFilter('date', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+
+  return {
+    templateFormats: [
+      "html",
+      "md"
+    ]
   };
+};
